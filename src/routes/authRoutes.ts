b1caa64871a0fcc856f11110cd5cc41bd423a453
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-10 16:49:09 -0600

    agregando el controlador y validación

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,31 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import { body } from 'express-validator'
+import { AuthController } from '../controllers/AuthControllers'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('desde /api/auth')
-})
+router.post('/create-account',
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('password')
+        .isLength({min: 8}).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, {req}) => {
+        if(value !== req.body.password) {
+            throw new Error('Los Password no son iguales')
+        }
+        return true
+    }),
+    body('email')
+        .isEmail().withMessage('Email no válido'),
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 export default router
+
+
+
+
 
 
