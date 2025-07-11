3bd1cfa8d1054672ebc69bcb7c1a3e562c804f31
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-11 15:24:27 -0600

    agregando la funcion generar token de 6 digitos

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express';
 import User from '../models/User';
 import { hashPassword } from '../utils/auth';
+import Token from '../models/Token';
+import { generateToken } from '../utils/token';
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response): Promise<void> => {
@@ -16,9 +18,19 @@ export class AuthController {
 
       // Crear usuario
       const user = new User(req.body);
+
       // Hash Password
       user.password = await hashPassword(password);
-      await user.save();
+
+      // Generar Token
+      const token = new Token()
+      token.token = generateToken()
+      token.user = user.id
+
+      // almacenar en la base de datos
+      await Promise.allSettled([user.save(), token.save()])
+     
+
       res.status(201).send('Cuenta creada, revisa tu email para confirmarla');
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' });
