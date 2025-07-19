ef09a90d99a74f73b305e7c95a8ac2337c150a57
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-19 12:37:22 -0600

    agregando middleware para autenticar

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -8,7 +8,7 @@ export class ProjectController {
     static createProject = async (req: Request, res: Response) => {
         const project = new Project(req.body)
 
-
+        console.log(req.user)
         try {
             await project.save()
             res.send('proyecto creado correctamente')
