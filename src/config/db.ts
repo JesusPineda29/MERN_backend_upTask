0135ee81fbbaa66b84c3c03e94f9ccf6121d7847
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-18 13:38:08 -0600

    agregando console para debug

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,7 @@ import { exit } from 'node:process';
 
 export const connectDB = async () => {
     try {
+        console.log("DATA_BASE_URL:", process.env.DATA_BASE_URL);
         const connection = await mongoose.connect(process.env.DATA_BASE_URL!)
         const url = `${connection.connection.host}:${connection.connection.port}`
         console.log(colors.bgCyan.bold(`mongoDB Conectado en: ${url}`))
