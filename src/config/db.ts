bbb6a5fb684506ea73327d98618a2556bc80bd8f
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-18 13:56:44 -0600

    continuando el debug

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,13 +3,11 @@ import colors from "colors";
 import { exit } from 'node:process';
 
 export const connectDB = async () => {
-    try {
-        console.log("DATA_BASE_URL:", process.env.DATA_BASE_URL);
-        const connection = await mongoose.connect(process.env.DATA_BASE_URL!)
-        const url = `${connection.connection.host}:${connection.connection.port}`
-        console.log(colors.bgCyan.bold(`mongoDB Conectado en: ${url}`))
-    } catch (error) {
-        console.log(colors.red.bold(`Error al conectar a MongoDB`))
-        exit(1)
-    }
+  try {
+    const connection = await mongoose.connect(process.env.DATA_BASE_URL!)
+    console.log("MongoDB conectado:", connection.connection.host)
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error) // <--- muestra el error real
+    process.exit(1)
+  }
 }
