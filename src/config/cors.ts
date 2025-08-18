9455d0e6f5fa7426d46ac6d05d906b3af77ce818
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-18 16:20:29 -0600

    arreglando error de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,18 +2,20 @@
 import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
-    origin: function (origin, callback) {
-        
-        
+  origin: function (origin, callback) {
+    // Creamos la lista de dominios permitidos
+    const whitelist = [process.env.FRONTEND_URL];
 
-        const whitelist = [process.env.FRONTEND_URL]
-        if(process.argv[2] = '--api') {
-            whitelist.push(undefined)
-        }
-        if(whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+    // Permitir requests sin origin (Postman, Node o pruebas locales)
+    if (process.argv[2] === '--api') {
+      whitelist.push(undefined);
     }
-}
+
+    // Validar el origin de la petición
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  }
+};
